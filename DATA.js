a77f8de9e4d85706742e46a929054694559726e0
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-04 04:09:50 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "785bdd4e-a75c-42fd-b02a-b768a0e927f3",
+    "home_feed": [
+        {
+            "id": "5af8c165-5efa-4803-b4c9-358a253f4722",
+            "artist_name": "fear",
+            "home_carousel": [
+                {
+                    "id": "69369a33-b90b-4b1b-8852-798950e4949e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/12.png"
+                },
+                {
+                    "id": "f5c21db6-5d7b-4724-af94-29adcf1ca59f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/329.png"
+                },
+                {
+                    "id": "fa944f51-1f00-4756-8cb9-875fb4f55299",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/205.png"
+                },
+                {
+                    "id": "84022fa1-e66f-4620-ba57-37ec2eeea851",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/364.png"
+                },
+                {
+                    "id": "1e3d6457-7120-49b9-8e68-cea751ef97eb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/337.png"
+                }
+            ],
+            "art_caption": "parent",
+            "art_description": "Claim deal television floor central strategy necessary. Act near memory fast nearly young. Brother write production likely blood partner."
+        },
+        {
+            "id": "d2ae141f-82d4-4b4f-9920-852083992e5c",
+            "artist_name": "act",
+            "home_carousel": [
+                {
+                    "id": "21e37065-45dd-4326-ac97-e965618ebd95",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/315.png"
+                },
+                {
+                    "id": "4ae79da8-93d0-4612-8311-b1c340d0d6d2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/364.png"
+                },
+                {
+                    "id": "fca12da3-4df0-447b-9e11-f8a936b415fe",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/97.png"
+                },
+                {
+                    "id": "c4db283d-08de-440a-90ca-5119224c943e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/134.png"
+                },
+                {
+                    "id": "6ce1cadd-8724-4a14-9843-cb1e5f9c264a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/173.png"
+                }
+            ],
+            "art_caption": "quickly",
+            "art_description": "Along probably concern happen form. Represent young media discover peace great."
+        },
+        {
+            "id": "84ad39fd-e6e5-4fdf-8156-83e5e243925f",
+            "artist_name": "rich",
+            "home_carousel": [
+                {
+                    "id": "14885901-7293-468f-8491-2917edaa055b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/225.png"
+                },
+                {
+                    "id": "8ec30764-afaf-4a63-a027-59eb07e0ae0b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/149.png"
+                },
+                {
+                    "id": "a4854a30-1b59-4d2d-8f30-411fb47c06e2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/246.png"
+                },
+                {
+                    "id": "184cfc36-7f3b-4d36-bb15-addbc31ce3d9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/269.png"
+                },
+                {
+                    "id": "a9d7f00a-397d-486b-a84b-516d789c5f54",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/206.png"
+                }
+            ],
+            "art_caption": "especially",
+            "art_description": "Letter evening college success stop have best process. Industry remember church have eat."
+        },
+        {
+            "id": "838806d8-00c6-4661-9dbb-89181c07b68f",
+            "artist_name": "lay",
+            "home_carousel": [
+                {
+                    "id": "4e4f9bcc-c6c5-438e-9662-566e9377d237",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/317.png"
+                },
+                {
+                    "id": "cc4116a4-2fda-4fda-931e-6276538ab174",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/336.png"
+                },
+                {
+                    "id": "761d5038-b004-4675-9998-6c96d153ddf5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/327.png"
+                },
+                {
+                    "id": "17c373b9-ff17-4134-b8d9-17e127de9927",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/367.png"
+                },
+                {
+                    "id": "92354d14-d903-4f4d-81d3-8931e5245c72",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/30.png"
+                }
+            ],
+            "art_caption": "water",
+            "art_description": "Rest trip take young. Politics visit have evening kid. Everyone force thing star per community."
+        },
+        {
+            "id": "ff377d51-88cb-4a2b-874f-86024fa0af00",
+            "artist_name": "opportunity",
+            "home_carousel": [
+                {
+                    "id": "9fdad2a5-ffca-4f9c-b503-e157dba4f83d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/38.png"
+                },
+                {
+                    "id": "42a7f3c6-58c5-4c48-b955-50d08e3ce471",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/234.png"
+                },
+                {
+                    "id": "1ae05876-4fbc-4bad-9b24-ac065c3a6f7c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/9.png"
+                },
+                {
+                    "id": "0540143c-4173-4767-a3ba-6d633e21ff3d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/55.png"
+                },
+                {
+                    "id": "5984bd41-535b-4e01-af13-8cc498cb2571",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/347.png"
+                }
+            ],
+            "art_caption": "sure",
+            "art_description": "Second kid war all there poor right. Serve item performance value raise too. Everybody control sound begin present doctor commercial."
+        }
+    ]
+}
